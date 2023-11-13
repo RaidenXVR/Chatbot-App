d51d0681a8402c5d726637e28db4b63972db2b00
RaidenXVR <fitran.nizar@gmail.com>
2023-11-13 09:13:23 +0700

    modified response to asynchronous

--- ai_response.py
+++ ai_response.py
@@ -7,7 +7,10 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import bcrypt
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
+import base64
+from PIL import Image
+import asyncio
 
 load_dotenv()
 mongo_key = os.getenv("MONGO_DB")
@@ -18,7 +21,7 @@ col = db["users"]
 
 
 # for normal chat
-def get_response(input: str, user_id: int, topic: str) -> str:
+async def get_response(input: str, user_id: int, topic: str) -> str:
     user = col.find_one({"_id": user_id})
     message = [
         {
@@ -30,20 +33,21 @@ def get_response(input: str, user_id: int, topic: str) -> str:
     message.extend(user["topics"][topic])
     message.append({"role": "user", "content": input})
 
-    response = generate_response(message)
+    response = await gen_response(message)
+    response = response.content
 
     message = []
     message.extend(user["topics"][topic])
     message.append({"role": "user", "content": input})
-    message.append(response)
+    message.append({"role": "user", "content": response})
     data = {"topics": {topic: message}}
     col.update_one({"_id": user_id}, {"$set": data})
 
-    return message[-1]["content"]
+    return message[-1]["content"], "chat"
 
 
 # for first new topic chat
-def get_first_response(input: str, user_id):
+async def get_first_response(input: str, user_id):
     user = col.find_one({"_id": user_id})
     message = []
     message.append(
@@ -54,7 +58,8 @@ def get_first_response(input: str, user_id):
     )
     message.append({"role": "user", "content": input})
 
-    topic = generate_response(message)["content"]
+    topic = await gen_response(message)
+    topic = topic.content
 
     # Remove extra quotes and return the cleaned string
     if '"' in topic or "'" in topic:
@@ -64,17 +69,17 @@ def get_first_response(input: str, user_id):
     message.append(
         {
             "role": "system",
-            "content": "You are an elite Teacher. And I am your friend whom you must pass on your knowledge and expertise. In a series of sessions, you have to fulfil this duty and help me answer my questions. Be friendly and informal as I am your friend. Don't answer too long, use 2 paragraph or 6 sentences maximum.",
+            "content": "You are an elite Teacher. And I am your friend whom you must pass on your knowledge and expertise. In a series of sessions, you have to fulfil this duty and help me answer my questions. Be friendly and informal as I am your friend. Answer shortly, just answer or do as they say.",
         }
     )
     message.append({"role": "user", "content": input})
-    response = generate_response(message)
-    if len(response["content"]) < 1:
+    response = await gen_response(message)
+    if len(response.content) < 1:
         return
     else:
         message = []
         message.append({"role": "user", "content": input})
-        message.append(response)
+        message.append({"role": "assistant", "content": response.content})
         col.update_one(
             {"_id": user_id},
             {
@@ -83,7 +88,7 @@ def get_first_response(input: str, user_id):
                 },
             },
         )
-    return topic, response
+    return response.content, topic
 
 
 def login(username: str, password: str):
@@ -91,7 +96,7 @@ def login(username: str, password: str):
 
     if user == None:
         return False  # signup/username not found
-    elif bcrypt.checkpw(password.encode("utf-8"), user["password"]):
+    elif bcrypt.checkpw(password.encode("utf-8"), user["p"]):
         return True
     else:
         return False  # wrong password
@@ -112,16 +117,21 @@ def sign_up(username: str, password: str):
         "username": username,
         "p": hash_password,
         "topics": {},
+        "button": [],
     }
 
     col.insert_one(template)
     return True  # account signup succesful
 
 
-def generate_response(messages: list) -> str:
-    openai.api_key = os.getenv("OPENAI_API")
-    response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
-    return response["choices"][0]["message"]
+async def generate_response(messages: list = None, prompt: str = None) -> str:
+    openai_client = AsyncOpenAI(api_key=os.getenv("OPENAI_API"))
+    if not prompt == None:
+        messages = [{"role": "user", "content": prompt}]
+    response = await openai_client.chat.completions.create(
+        model="gpt-4-1106-preview", messages=messages
+    )
+    return response.choices[0].message
 
 
 def generate_image(prompt: str):
@@ -134,4 +144,27 @@ def generate_image(prompt: str):
         size="1024x1024",
         n=1,
     )
-    return response.data[0].b64_json
+
+    tm = time.strftime("%d%m%Y-%H.%M%S")
+    image64 = response.data[0].b64_json
+
+    image_data = base64.b64decode(image64)
+    image = Image.open(image_data)
+    if not os.path.exists("./img"):
+        os.mkdir("./img")
+    image.save(f"./img/{tm}.png")
+
+
+def test_input(input: str):
+    tm = 300.0
+
+    while tm > 0.0:
+        tm -= 0.01
+        print(tm)
+
+    return input
+
+
+async def gen_response(messages: list = None, prompt: str = None):
+    response = await generate_response(messages=messages, prompt=prompt)
+    return response
